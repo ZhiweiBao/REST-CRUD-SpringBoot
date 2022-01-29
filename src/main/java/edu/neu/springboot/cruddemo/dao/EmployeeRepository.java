16b09323ffcae3b594ab4373d5b52bf15712d68b
Zhiwei Bao <zhiv.bao@gmail.com>
2022-01-29 15:45:56 -0800

    Refactor REST CRUD API with Spring Data REST

--- src/main/java/edu/neu/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/edu/neu/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package edu.neu.springboot.cruddemo.dao;
 
 import edu.neu.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
